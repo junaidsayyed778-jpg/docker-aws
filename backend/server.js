7e04a82d51967042a2ebbf93b0a3517d606342d8
Junaid Sayyed <junaidsayyed778@gmail.com>
2026-04-05 18:15:30 +0530

    add socket io

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { YSocketIO } from "y-socket.io/dist/server"
+
+const app = express();
+
+const httpServer = createServer(app);
+app.use(cors());
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize();
+
+ app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
+    success: true,
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    message: "pk",
+    success: true,
+  });
+});
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Editor } from "@monaco-editor/react";
+import { useRef, useMemo } from "react";
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
+import { MonacoBinding } from "y-monaco";
 
 const App = () => {
-  return (
-    <div>
-      hi
-    </div>
-  )
-}
+  const editorRef = useRef(null);
 
-export default App
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco",
+      ydoc,{
+        autoConnect: true,
+      },
+    );
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness,
+    );
+  };
+  return (
+    <main className="h-screen w-full bg-gray-950 flex gap-4 p-2">
+      <aside className="h-full w-2/5 bg-amber-100 rounded-md"></aside>
+      <section className="h-full w-3/4 bg-neutral-800">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme="vs-dark"
+          onMount={handleMount}
+        />
+      </section>
+    </main>
+  );
+};
+
+export default App;
